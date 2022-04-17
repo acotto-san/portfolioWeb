ab2a2c006da073818170be9b99c22e04d314651a
acottosan <acostaottonelli.santiago@gmail.com>
2022-04-17 02:57:55 -0300

    comienzo de data binding en app-datos-personales para el aboutMeParagraph

--- src/app/components/datos-personales/datos-personales.component.ts
+++ src/app/components/datos-personales/datos-personales.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './datos-personales.component.html',
   styleUrls: ['./datos-personales.component.css']
 })
-export class DatosPersonalesComponent implements OnInit {
 
-  constructor() { }
+
+export class DatosPersonalesComponent implements OnInit {
+  
+  aboutMeParagraph? : string ;
+
+
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    this.aboutMeParagraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi veritatis explicabo ut alias laboriosam dolores similique ex quam. Esse commodi error consequuntur enim assumenda architecto quidem rerum ipsa nemo quia unde natus ad nesciunt reprehenderit fugit, numquam, vitae itaque. Eligendi?"
   }
-
 }
