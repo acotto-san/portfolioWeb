3972b02636c82b9adbcfbcf4757cc8466357c606
acotto-san <acostaottonelli.santiago@gmail.com>
2022-04-20 01:56:28 -0300

    listo habia que importar HttpClientModule en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,6 +49,7 @@ import { TarjetaComponent } from './seccion/tarjetas/tarjeta/tarjeta.component';
     TarjetaComponent,
   ],
   imports: [
+    HttpClientModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule
